a13d8db29251b029c7214d5cc43139aae92742c7
Matti Paksula <matti.paksula@iki.fi>
2014-05-25 15:33:44 +0300

    roundings

--- app/models/result.rb
+++ app/models/result.rb
@@ -5,13 +5,13 @@ class Result < ActiveRecord::Base
 
   def delta_webview_to_native
     delta = native_received_at.to_f - webview_started_at.to_f
-    delta < 0 ? "N/A" : delta
+    delta < 0 ? "N/A" : delta.round(4)
 
   end
 
   def delta_native_to_webview
     delta = webview_received_at.to_f - native_started_at.to_f
-    delta < 0 ? "N/A" : delta
+    delta < 0 ? "N/A" : delta.round(4)
   end
 
 end
