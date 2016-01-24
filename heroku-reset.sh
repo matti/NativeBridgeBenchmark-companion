6e24e4b937d51a269c7cf178b546b39810c4c288
Matti Paksula <matti.paksula@iki.fi>
2016-01-24 09:12:46 +0200

    lel

--- heroku-reset.sh
+++ heroku-reset.sh
@@ -1,4 +1,4 @@
-heroku pg:reset postgresql-dimensional-6583 --confirm nativebridge-benchmark --app nativebridge-benchmark
+#heroku pg:reset postgresql-dimensional-6583 --confirm nativebridge-benchmark --app nativebridge-benchmark
 heroku pg:reset DATABASE_URL --confirm nativebridge-benchmark
 heroku run "rake db:migrate" --app nativebridge-benchmark
 
