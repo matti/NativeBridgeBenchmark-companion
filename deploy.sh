807ee66cd6d1f6c03196a9e072ba84275a3478dc
Matti Paksula <matti.paksula@iki.fi>
2016-02-24 20:19:27 +0200

    multiheroku

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,79 @@
 #!/usr/bin/env sh
 
+function remoteName() {
+  basename $1
+}
+
+function herokuName() {
+  echo $(remoteName $1) | rev | cut -f 2- -d '.' | rev
+}
+
+function herokuCreate() {
+  heroku apps:create $(herokuName $1)
+}
+
+function herokuDelete() {
+  heroku apps:destroy $(herokuName $1) --confirm $(herokuName $1)
+}
+
+function addPostgres() {
+  heroku addons:create heroku-postgresql:hobby-basic --app $(herokuName $1)
+}
+
+function pushNMigrate() {
+  git remote rm $(remoteName $1)
+  git remote add $(remoteName $1) $1
+
+  git push $(remoteName $1) master
+  heroku run 'rake db:migrate' --app $(herokuName $1)
+}
+
+function seedTests() {
+  case $(herokuName $1) in
+  "nb-uiwebview-native")
+    ./heroku-reset.sh $(herokuName $1) uiwebview native suite
+    ;;
+  "nb-native-uiwebview")
+    ./heroku-reset.sh $(herokuName $1) uiwebview webview suite
+    ;;
+  "nb-wkwebview-native")
+    ./heroku-reset.sh $(herokuName $1) wkwebview native suite
+    ;;
+  "nb-native-wkwebview")
+    ./heroku-reset.sh $(herokuName $1) wkwebview webview suite
+    ;;
+  "*")
+    echo "no idea!"
+    exit 1
+  esac
+}
+
+read -r -d "\n" REMOTES << EOF
+https://git.heroku.com/nb-uiwebview-native.git
+https://git.heroku.com/nb-native-uiwebview.git
+https://git.heroku.com/nb-wkwebview-native.git
+https://git.heroku.com/nb-native-wkwebview.git
+EOF
+
+IFS="
+"
+
+
+
+for REMOTE in $REMOTES; do
+  #herokuDelete $REMOTE
+  #herokuCreate $REMOTE
+  #addPostgres $REMOTE
+  #pushNMigrate $REMOTE
+  seedTests $REMOTE
+done
+
+exit 1
+
 git remote add heroku git@heroku.com:nativebridge-benchmark.git
 
 heroku maintenance:on
 
-git push heroku master &&
-heroku run 'rake db:migrate'
+
 
 heroku maintenance:off
--- heroku-reset.sh
+++ heroku-reset.sh
@@ -1,6 +1,5 @@
 #heroku pg:reset postgresql-dimensional-6583 --confirm nativebridge-benchmark --app nativebridge-benchmark
-heroku pg:reset DATABASE_URL --confirm nativebridge-benchmark
-heroku run "rake db:migrate" --app nativebridge-benchmark
+heroku pg:reset DATABASE_URL --confirm $1
+heroku run "rake db:migrate" --app $1
 
-heroku run rake db:seed $1 $2 $3
-
+heroku run rake db:seed $2 $3 $4 --app $1
