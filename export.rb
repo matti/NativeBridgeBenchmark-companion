2c1637690c438e56644ff096a4da3b3405793c6b
Matti Paksula <matti.paksula@iki.fi>
2016-02-27 21:08:17 +0200

    pg dev, export

--- export.rb
+++ export.rb
@@ -10,9 +10,10 @@ require "csv"
 fields = ["id", "method", "fps", "mem", "cpu", "w2n", "n2w", "pause", "agent"]
 
 known_agents = {
-  "Mozilla/5.0 (iPad; CPU OS 9_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13C752" => "ipadmini4",
+  #"Mozilla/5.0 (iPad; CPU OS 9_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13C752" => "ipadmini4",
   "Mozilla/5.0 (iPad; CPU OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13D151" => "ipadmini", #updated
-  "Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13D152" => "iphone5s" #updated
+  "Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13D152" => "iphone6plus", #updated
+  "Mozilla/5.0 (iPad; CPU OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13D152" => "ipad3" #koska 2
 }
 
 
@@ -56,18 +57,26 @@ module Enumerable
 
 end
 
+def format(str)
+  "#{str}\n"
+end
+
+debuggen = false
+
+test_stats_output = ""
 agents.each do |agent|
+  test_stats_output << format("--[ #{known_agents[agent]} ]--------------")
   payloads.each do |payload|
     csv_string = CSV.generate(col_sep: ",") do |csv|
       csv << header_fields
 
+
       Test.all.each do |test|
         test_method_name, test_amount, test_interval, test_payload, others = test.name.split("-")
+
+        next unless test_payload == payload
+
         test_configuration = "test-#{test_amount}-#{test_interval}-#{test_payload}"
-
-        puts "test: #{test.name}"
-        puts "results: #{test.results.size}"
-
         results_for_agent = test.results.where(agent: agent)
 
         pause_samples = results_for_agent.map(&:render_paused)
@@ -82,27 +91,33 @@ agents.each do |agent|
         native_to_webview_ms_delta_average = native_to_webview_ms_delta_samples.mean
         native_to_webview_ms_delta_stddev = native_to_webview_ms_delta_samples.standard_deviation
 
+
+        test_stats_output << format("#{test.name}")
+
+        results_written = 0
         results_for_agent.each do |result|
+          outlier_multiplier = 6
           current_pause_absolute_distance = (result.render_paused - pause_average).abs
           current_native_to_webview_ms_delta_absolute_distance = (result.native_to_webview_ms_delta - native_to_webview_ms_delta_average).abs
           current_webview_to_native_ms_delta_absolute_distance = (result.webview_to_native_ms_delta - webview_to_native_ms_delta_average).abs
 
-          outlier_by_pause = current_pause_absolute_distance > 2*pause_stddev
-          outlier_by_webview_to_native = current_webview_to_native_ms_delta_absolute_distance > 2*webview_to_native_ms_delta_stddev
-          outlier_by_native_to_webview = current_native_to_webview_ms_delta_absolute_distance > 2*native_to_webview_ms_delta_stddev
+          outlier_by_pause = (result.render_paused > pause_average) && current_pause_absolute_distance > outlier_multiplier*pause_stddev
+          outlier_by_webview_to_native = (result.webview_to_native_ms_delta > webview_to_native_ms_delta_average) && current_webview_to_native_ms_delta_absolute_distance > outlier_multiplier*webview_to_native_ms_delta_stddev
+          outlier_by_native_to_webview = (result.native_to_webview_ms_delta > native_to_webview_ms_delta_average) && current_native_to_webview_ms_delta_absolute_distance > outlier_multiplier*native_to_webview_ms_delta_stddev
 
+          puts "#{result.render_paused} - #{result.webview_to_native_ms_delta} - #{result.native_to_webview_ms_delta}" if debuggen
           if outlier_by_pause ||
              outlier_by_webview_to_native ||
              outlier_by_native_to_webview
 
             print "OUTLIER by: "
-            puts "pause" if outlier_by_pause
-            puts "webview to native" if outlier_by_webview_to_native
-            puts "native to webview" if outlier_by_native_to_webview
+            puts "pause #{result.render_paused}" if outlier_by_pause
+            puts "webview to native #{result.webview_to_native_ms_delta}" if outlier_by_webview_to_native
+            puts "native to webview #{result.native_to_webview_ms_delta}" if outlier_by_native_to_webview
 
             next
           else
-            puts "not outleir"
+            #
           end
 
           result_row = []
@@ -133,13 +148,17 @@ agents.each do |agent|
             end
           end
 
-          csv << result_row if agent == result.agent && payload == test_payload
-
+          csv << result_row
+          results_written += 1
         end
+        test_stats_output << format("  total results wo/ outliers: #{results_written}")
 
-      end
-    end
+      end #test
 
+    end #csv
     File.write("tmp/#{known_agents[agent]}-#{export_name}-#{payload}.csv", csv_string)
-  end
-end
+  end #payloads
+
+  File.write("tmp/#{known_agents[agent]}-#{export_name}.txt", test_stats_output)
+  test_stats_output=""
+end # agents
